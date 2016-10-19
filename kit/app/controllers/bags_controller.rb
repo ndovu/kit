30c143d0f1209b8cf1a6d9c3f60047cf4262b9df
stressedesserts <noel.bacani@gmail.com>
2016-10-18 18:57:42 -0700

    created preliminary views for kit model

--- kit/app/controllers/bags_controller.rb
+++ kit/app/controllers/bags_controller.rb
@@ -1,17 +1,26 @@
 class BagsController < ApplicationController
   def new
+    @bag = Bag.new
   end
 
   def create
     @bag = Bag.new(bag_params)
- 
+    
     @bag.save
     redirect_to @bag
+  end
+
+  def show
+    @bag = Bag.find(params[:id])
+  end
+
+  def index
+    @bags = Bag.all
   end
  
   private
     def bag_params
-      params.require(:name).permit(:description, :link)
+      params.require(:bag).permit(:adv, :description, :link)
     end
-    
+
 end
